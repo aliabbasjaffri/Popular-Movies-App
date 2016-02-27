868a1b16dd5151349b5f903f3550f06545adb56d
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-02-28 02:41:32 +0500

    Movies and Genres Contract Setup in native.

---
+++ app/src/main/java/com/popularmoviesapp/application/MovieApplication.java
@@ -0,0 +1,15 @@
+package com.popularmoviesapp.application;
+
+import android.app.Application;
+
+/**
+ * Created by aliabbasjaffri on 28/02/16.
+ */
+public class MovieApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
