eac78849dc8ff7e7c12fd0bd0e144768405e1a8c
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-03-17 21:41:16 +0500

    General Refactoring.

--- app/src/main/java/com/popularmoviesapp/activity/MainActivity.java
+++ app/src/main/java/com/popularmoviesapp/activity/MainActivity.java
@@ -1,21 +1,14 @@
 package com.popularmoviesapp.activity;
 
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.popularmoviesapp.R;
-import com.popularmoviesapp.provider.DatabaseHelper;
-import com.popularmoviesapp.provider.MovieProvider;
-import com.popularmoviesapp.utils.Constants;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
