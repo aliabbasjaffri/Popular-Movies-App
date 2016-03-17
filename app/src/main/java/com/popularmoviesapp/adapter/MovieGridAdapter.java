6084a4aac5da4b4488f313eae11e0c9ead73c8b7
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-03-18 01:41:07 +0500

    General Edits. Working towards Movie Grid Adapter.

--- app/src/main/java/com/popularmoviesapp/adapter/MovieGridAdapter.java
+++ app/src/main/java/com/popularmoviesapp/adapter/MovieGridAdapter.java
@@ -2,30 +2,71 @@ package com.popularmoviesapp.adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.TextView;
+import com.popularmoviesapp.R;
+import butterknife.Bind;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import butterknife.ButterKnife;
+import android.widget.ImageView;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by aliabbasjaffri on 17/03/16.
  */
-public class MovieGridAdapter extends BaseAdapter
+public class MovieGridAdapter extends RecyclerView.Adapter<MovieGridAdapter.ViewHolder>
 {
-    @Override
-    public int getCount() {
-        return 0;
+    private List<String> movieList = new ArrayList<>();
+    private boolean mFavoriteView;
+    private Calendar mCalendar;
+
+    public MovieGridAdapter()
+    {
+
     }
 
     @Override
-    public Object getItem(int position) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        @Bind(R.id.gridMovieItemFrameLayout)
+        FrameLayout mGridMovieItemContainer;
+
+        @Bind(R.id.gridMovieItemImageView)
+        ImageView mGridMovieItemImageView;
+
+        @Bind(R.id.gridMovieItemLikeImageView)
+        ImageView mGridMovieItemLikeImageView;
+
+        @Bind(R.id.gridMovieItemAverageRating)
+        TextView mGridMovieItemAverageRating;
+
+        @Bind(R.id.gridMovieItemYear)
+        TextView mGridMovieItemDate;
+
+        @Bind(R.id.gridMovieItemTitleContainer)
+        LinearLayout mGridMovieItemTitleContainer;
+
+        public ViewHolder(View view)
+        {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
     }
 }
