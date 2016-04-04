61326280503c3eaacaf65142dac51f2beec0e4fc
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-04-05 00:27:35 +0500

    Partial Commit 2 - 2

--- app/src/main/java/com/popularmoviesapp/activity/MainActivity.java
+++ app/src/main/java/com/popularmoviesapp/activity/MainActivity.java
@@ -46,6 +46,26 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
                         .setAction("Action", null).show();
             }
         });
+
+        if (findViewById(R.id.movie_detail_container) != null)
+        {
+            // The detail container view will be present only in the large-screen layouts
+            // (res/layout-sw600dp). If this view is present, then the activity should be
+            // in two-pane mode.
+            mTwoPane = true;
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            if (savedInstanceState == null)
+            {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.movie_detail_container, new DetailActivityFragment(), DETAILFRAGMENT_TAG)
+                        .commit();
+            }
+        }
+        else
+            mTwoPane = false;
+
         MoviesSyncAdapter.initializeSyncAdapter(this);
     }
 
