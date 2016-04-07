30f0a9960e55dec7bc3dc4fd49dcddc9112dede6
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-04-08 00:58:33 +0500

    Setting up detail fragment.

--- app/src/main/java/com/popularmoviesapp/fragment/DetailActivityFragment.java
+++ app/src/main/java/com/popularmoviesapp/fragment/DetailActivityFragment.java
@@ -5,22 +5,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.popularmoviesapp.R;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DetailActivityFragment extends Fragment {
-
+public class DetailActivityFragment extends Fragment
+{
     public static final String DETAIL_URI = "URI";
+
+    ImageView backDropImage;
+    ImageView posterImage;
+    ImageView trailerPlayButtonImage;
+    ImageView favoriteButtonImage;
+    TextView movieTitle;
+    TextView movieReleaseDate;
+    TextView movieRating;
+    TextView movieOverview;
 
     public DetailActivityFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        backDropImage = (ImageView) view.findViewById(R.id.fragmentDetailsMovieBackDropImage);
+        posterImage = (ImageView) view.findViewById(R.id.fragmentDetailsMoviePosterImage);
+        trailerPlayButtonImage = (ImageView) view.findViewById(R.id.fragmentDetailsMovieTrailerPlayButton);
+        favoriteButtonImage = (ImageView) view.findViewById(R.id.fragmentDetailsMovieFavoriteImage);
+        movieTitle = (TextView) view.findViewById(R.id.fragmentDetailsMovieTitleText);
+        movieReleaseDate = (TextView) view.findViewById(R.id.fragmentDetailsMovieReleaseDateText);
+        movieRating = (TextView) view.findViewById(R.id.fragmentDetailsMovieRatingText);
+        movieOverview = (TextView) view.findViewById(R.id.fragmentDetailsMovieOverviewText);
+
+        return view;
     }
 }
